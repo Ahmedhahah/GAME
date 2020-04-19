6b8b0ffafd230316e28f2d31cf5d0dc6e9a9b72f
yassinejrad <60968382+yassinejrad@users.noreply.github.com>
2020-04-19 10:56:05 +0100

    Update map.h

--- map.h
+++ map.h
@@ -6,7 +6,6 @@ typedef struct map
 	SDL_Surface *image[15];
 };
 void afficher_seconde_entite(med m[],boss b[],wiseman w,map background);
-void afficher_enigme(enigme *e[] ,int stage,map background,wiseman w);
 void afficher_personnage(Will hero ,boss *b[], med *m[],int stage,map background,wiseman w);
 int collision_entre_personnage_et_background(Will *hero, int stage,map background);
 void scrolling(Will hero ,map background);
