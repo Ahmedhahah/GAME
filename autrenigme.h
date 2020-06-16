246aef55658946dba8ff02bb235e7def58fd13e8
youssefzahar <61435160+youssefzahar@users.noreply.github.com>
2020-06-16 11:50:44 +0200

    Add files via upload

---
+++ autrenigme.h
@@ -0,0 +1,20 @@
+#ifndef AUTRENIGME_H_INCLUDED
+#define AUTRENIGME_H_INCLUDED
+
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <stdbool.h>
+#include <math.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_ttf.h>
+#include <SDL/SDL_mixer.h>
+
+SDL_Surface *screen;
+
+
+int autre_enigme(SDL_Surface *screen);
+
+#endif // AUTRENIGME_H_INCLUDED
