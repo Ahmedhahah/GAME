7c4ef142d198b418b43f8d1f2afd2d2f9bf5461d
wassimmm <61117123+wassimmm@users.noreply.github.com>
2020-06-16 11:41:57 +0200

    Update time.h

--- time.h
+++ time.h
@@ -1,3 +1,14 @@
+
+/**
+* @file time.h
+* @brief Testing Program.
+* @author C Team
+* @version 0.1
+* @date Apr 01, 2015
+*
+* structure perso
+*
+*/
 #ifndef TIME_H_INCLUDED
 #define TIME_H_INCLUDED
 #include <SDL/SDL.h>
@@ -7,16 +18,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "perso.h"
 
+/**
+* @struct time_perso
+* @brief struct for time
+*/
+typedef struct
+{
+  
+  TTF_Font* police_time;
+  SDL_Surface *texte_time;  /*!< Surface. */
+  SDL_Rect pos_texte_time; /*!< Rectangle*/
 
+} time_perso;
 
-int timeAct(int tempsActuel) ;
+void init_time(time_perso* t,int x);
 
+void afficher_time(personnage *p, SDL_Surface *screen, time_perso *t, int x);
 
+void save_time(int t) ;
+
+int get_time() ;
 
 int checktime(int tempsActuel , int tempsPrecedent);
 
-void pause();
+
 
 #endif
-
